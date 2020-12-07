591c49ddd7bb7faf3eefcbf24fed7cac9712fd9c
Anderson E. Peñaloza C <designeromega@gmail.com>
2020-12-07 14:48:22 +0100

    added SharedModule to start implementing shared reusable components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AngularMaterialModule } from "./angular-material.module";
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { AngularMaterialModule } from "./angular-material.module";
     AppRoutingModule,
     BrowserAnimationsModule,
 		AngularMaterialModule,
+		SharedModule,
   ],
   providers: [
 	],
